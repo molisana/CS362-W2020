509c771df3c4e5ff9ed0f3abf583db5c75631b82
Alex Molisani <molisana@oregonstate.edu>
2020-01-14 15:58:35 -0800

    finished refactoring

---
+++ projects/molisana/dominion/testDominion2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 14:39:32 2020
+
+@author: molisana
+"""
+
+import testUtility as refactor
+
+#Get player names
+player_names = ["Annie","*Ben","*Carla"]
+
+supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
+                3:['Silver','Chancellor','Village','Woodcutter','Workshop'],
+                4:['Gardens','Bureaucrat','Feast','Militia','Moneylender','Remodel','Smithy','Spy','Thief','Throne Room'],
+                5:['Duchy','Market','Council Room','Festival','Laboratory','Library','Mine','Witch'],
+                6:['Gold','Adventurer'],8:['Province']}
+
+nC, nV = refactor.numCursesVictory(player_names)
+
+supply = refactor.getSupply(nV)
+
+refactor.addSupplyStandard(supply, player_names, nV, nC)
+
+trash, players = refactor.initializeGame(player_names)
+
+refactor.playGame(supply_order, players, supply, trash)
+            
+refactor.calcFinalScore(players)
